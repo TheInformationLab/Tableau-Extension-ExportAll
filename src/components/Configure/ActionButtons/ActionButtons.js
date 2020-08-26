e3d59d05bdd8a307394ef07f046d6a49f6c97d46
Craig Bloodworth <craig.bloodworth@theinformationlab.co.uk>
2020-08-26 16:17:19 +0100

    Add extension version number to config screen

--- src/components/Configure/ActionButtons/ActionButtons.js
+++ src/components/Configure/ActionButtons/ActionButtons.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { Button } from '@tableau/tableau-ui';
+import packageJson from '../../../../package.json';
 
 const placeButtons = {
   bottom: 10,
@@ -7,12 +8,23 @@ const placeButtons = {
   position: 'fixed'
 }
 
+const version = {
+  left: 10,
+  bottom: 10,
+  position: 'fixed',
+  color: '#4f4f4f',
+  fontSize: 12
+}
+
 function ActionButtons(props) {
 
   return (
-    <div style={placeButtons}>
-      <Button kind={'outline'} style={{marginRight: 12}} onClick={props.resetSettings}>Reset</Button>
-      <Button kind={'primary'} style={{marginRight: 12}} disabled={!props.enableButton} onClick={props.save}>Save Changes</Button>
+    <div>
+      <div style={version}>v{packageJson.version}</div>
+      <div style={placeButtons}>
+        <Button kind={'outline'} style={{marginRight: 12}} onClick={props.resetSettings}>Reset</Button>
+        <Button kind={'primary'} style={{marginRight: 12}} disabled={!props.enableButton} onClick={props.save}>Save Changes</Button>
+      </div>
     </div>
   )
 
